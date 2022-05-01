78a6e4fbecfc709979fb1c03d4ad53d9a2d4637c
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2022-05-01 20:44:32 +0900

    add examples next router

--- src/pages/news/[id]/index.tsx
+++ src/pages/news/[id]/index.tsx
@@ -1,6 +1,22 @@
+import { useRouter } from 'next/router'
+import { Children } from 'react'
+
 export type NewsDetailProps = {
-    children?: any,
-    href?: string
+  children?: any
+  href: string
 }
 
-export function NewsDetail(props: NewsDetailProps) {}
+export default function NewsDetail(props: NewsDetailProps) {
+  const router = useRouter()
+
+  const style = {
+    marginRight: 10,
+    color: router.asPath === props.href ? 'red' : 'black',
+  }
+
+  return (
+    <a href={props.href} style={style}>
+      <p>Hoge</p>
+    </a>
+  )
+}
